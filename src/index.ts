5abbcb7d7677014847f4010e60d3c67cb343730a
Ayan <ayan2572006@gmail.com>
2024-09-11 03:17:05 +0530

    DATABASE_URL ENV

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
 
-app.post("/api/v1/signup", (c) => {
+app.post("/api/v1/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
   return c.text("Signup");
 });
 app.post("/api/v1/signin", (c) => {
